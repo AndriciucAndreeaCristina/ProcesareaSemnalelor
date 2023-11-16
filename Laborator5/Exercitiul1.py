19f286081965eca3cdc185953a27962d88b5eaf6
AndriciucAndreeaCristina <andriciucandreea@yahoo.com>
2023-11-16 11:41:19 +0200

    lab 5 done

--- Laborator5/Exercitiul1.py
+++ Laborator5/Exercitiul1.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
-import pandas as pd
 
-x = np.genfromtxt("D:/facultate/ProcesareaSemnalelor/Laborator5/Train.csv", delimiter=',')
+
+x = np.genfromtxt("D:/facultate/ProcesareaSemnalelor/Laborator5/Train.csv", delimiter=',', skip_header=1)
 N = len(x)
 # a
 # avem cate o valoare pentru fiecare ora din zi, pentru 762 de zile, adica 1 an si o luna
@@ -11,38 +10,47 @@ N = len(x)
 # frecventa de esantionare este de 0,002766
 
 # b
-# esantionale din fisier acopera 762 de zile, adica 1 an si o luna
-# fs = 1/T => T = 361,445 secunde
+# esantionale din fisier acopera 762 de zile, adica 2 ani si o luna
 
 # c
 # stim ca un semnal este esantionat corect daca frecventa de esantionare este de cel putin dublul frecventei maxime a semnalului
 # frecventa de esantionare este 0.002766 Hz, deci frecventa maxima a semnalului poate fi maxim 0.001383
 
 # d
-fs = 0.002766
+fs = 1 / 3600
 signal = np.array(x[1:, 2])
-signal = [int(x) for x in signal]
 X = np.fft.fft(signal)
 X = abs(X/N)
-print(X)
+X = X[0:int(N/2)]
+f = fs * np.linspace(0, N/2, N//2)/N
 plt.plot(X)
 plt.yscale("log")
-plt.show()
-# observam ca valoarea modului este semnificativa pentru frecventa 0Hz, deci putem spune ca avem si o componenta continua
-f = fs*np.linspace(0, N, N-1)/N
-print(f)
-plt.plot(f, X)
-plt.yscale("log")
-plt.savefig("L5_1d.pdf", format="pdf", bbox_inches="tight")
-plt.savefig("L5_1d.png", format="png", bbox_inches="tight")
+# plt.savefig("L5_1d.pdf", format="pdf", bbox_inches="tight")
+# plt.savefig("L5_1d.png", format="png", bbox_inches="tight")
 plt.show()
 
 # e
+med = np.median(signal)
+if med != 0:
+    print("Semnalul are o componenta continua")
+    signal = signal - med
+else:
+    print("Semnalul nu are o componenta continua")
+# X = np.fft.fft(signal)
 
 # f
+max_indexes = np.argsort(X)[-4:]
+max_vals = X[max_indexes]
+max_freqs = f[max_indexes]
+
+print(max_vals)
+print(max_freqs)
 
 # g
-
-# h
-
-# i
+start = 1056
+month = x[start : start + 24 * 30]
+print(month[:,2])
+plt.plot(month[:,2], color = "red")
+plt.savefig("L5_1g.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("L5_1g.png", format="png", bbox_inches="tight")
+plt.show()
