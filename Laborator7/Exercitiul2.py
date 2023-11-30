2b501a5792c2ad3dff3a823eaddc3ea8793a0ac9
AndriciucAndreeaCristina <andriciucandreea@yahoo.com>
2023-11-30 14:45:03 +0200

    lab7 done

--- Laborator7/Exercitiul2.py
+++ Laborator7/Exercitiul2.py
@@ -6,5 +6,19 @@ X = misc.face(gray=True)
 plt.imshow(X, cmap=plt.cm.gray)
 plt.show()
 
-print(X.shape)
+X_fft = np.fft.fft2(X)
+freq_db = 20 * np.log10(np.abs(X_fft))
+plt.imshow(freq_db, cmap=plt.cm.gray)
+plt.show()
+
+SNR = 50
+freq_cutoff = np.max(freq_db) - SNR
+Y_cutoff = X_fft.copy()
+Y_cutoff[freq_db > freq_cutoff] = 0
+X_cutoff = np.fft.ifft2(Y_cutoff).real
+plt.imshow(X_cutoff, cmap=plt.cm.gray)
+plt.show()
+plt.colorbar()
+plt.savefig("Laborator7/L7_2.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("Laborator7/L7_2.png", format="png", bbox_inches="tight")
 
--- Laborator7/Exercitiul3.py
+++ Laborator7/Exercitiul3.py
@@ -1,3 +1,23 @@
 from scipy import misc, ndimage
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
+
+X = misc.face(gray=True)
+X_fft = np.fft.fft2(X)
+freq_db = 20 * np.log10(np.abs(X_fft))
+SNR = 50
+freq_cutoff = np.max(freq_db) - SNR
+Y_cutoff = X_fft.copy()
+Y_cutoff[freq_db > freq_cutoff] = 0
+X_cutoff = np.fft.ifft2(Y_cutoff).real
+
+noise = X_cutoff - X
+plt.imshow(noise, cmap=plt.cm.gray)
+plt.title("Noise")
+plt.show()
+
+SNR_X = 20 * np.log10(np.mean(X) / np.std(X))
+SNR_X_cutoff = 20 * np.log10(np.mean(X_cutoff) / np.std(X_cutoff))
+print("SNR_X: ", SNR_X)
+print("SNR_X_cutoff: ", SNR_X_cutoff)
